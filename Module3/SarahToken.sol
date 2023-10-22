5b5a5ff0addacaba38e06d4c48b09e0a82f97296
sarahannie <87903267+sarahannie@users.noreply.github.com>
2023-10-22 12:27:58 +0100

    Update SarahToken.sol

--- Module3/SarahToken.sol
+++ Module3/SarahToken.sol
@@ -1,22 +1,16 @@
 // SPDX-License-Identifier: MIT
 
-pragma solidity ^0.8.18;
+pragma solidity ^0.8.20;
 
-contract SarahToken {
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+contract SarahToken is ERC20{
 
     // public variables
-    string public tknName;
-    string public tknSymbol;
-    uint256 public totalSupply;
     address public owner;
 
-    // mapping of addresses to balances
-    mapping(address => uint256) public balance;
-
     //  set the token name, symbol, and owner
-    constructor() {
-        tknName = "Sarah Token";
-        tknSymbol = "SAH";
+    constructor() ERC20("Sarah Token", "SAH"){        
         owner = msg.sender;
     }
 
@@ -28,31 +22,26 @@ contract SarahToken {
 
     // this function allows only the owner of the contract to mint new tokens
     function mint(address _recipient, uint256 _amount) public onlyOwner {
-        // increase the total supply
-        totalSupply += _amount;
         // increase the balance of the specified address
-        balance[_recipient] += _amount;
+        _mint(_recipient, _amount);
     }
 
     // this function allows anyone to burn their own tokens
     function burn(uint256 _amount) public {
         // require that the sender has enough tokens to burn
-        require(balance[msg.sender] >= _amount, "Insufficient balance");
-        // subtract the burn amount from the total supply
-        totalSupply -= _amount;
-        // subtract the burn amount from the sender's balance
-        balance[msg.sender] -= _amount;
+        require(balanceOf(msg.sender) >= _amount, "Insufficient balance");
+        // subtract the burn amount from the sender's balance        
+        _burn(msg.sender, _amount);
     }
 
     // this allows anyone to transfer tokens to another address
-    function transfer(address _recipient, uint256 _amount) public {
+    function transfer(address _recipient, uint256 _amount) public override returns (bool) {
         // require that the sender is not the recipient
         require(msg.sender != _recipient,"You cannot transfer to yourself!");
         // require that the sender has enough tokens to transfer
-        require(balance[msg.sender] >= _amount, " Amount to transfer is greater than balance");
-        // subtract the transfer amount from the sender's balance
-        balance[msg.sender] -= _amount;
-        // add the transfer amount to the recipient's balance
-        balance[_recipient] += _amount;
+        require(balanceOf(msg.sender) >= _amount, " Amount to transfer is greater than balance");
+        // transfer to recipient account
+        _transfer(_msgSender(), _recipient, _amount);
+        return true;
     }
 }
